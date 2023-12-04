64ba651f9234075998553a05f1c9d69f50599319
ponchik009 <lolikonru@mail.ru>
2023-12-04 22:47:37 +0800

    draggable scroll cases

--- src/components/CasesBlock/CasesList/CasesList.tsx
+++ src/components/CasesBlock/CasesList/CasesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
 
@@ -27,11 +27,88 @@ export const CasesList: React.FC<ICasesListProps> = ({
   cases,
   ...props
 }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState<number | null>(null);
+  const [scrollLeft, setScrollLeft] = useState<number | null>(null);
+
+  const [velX, setVelX] = useState(0);
+  const [momentumID, setMomentumID] = useState<number | null>(null);
+
+  function beginMomentumTracking() {
+    cancelMomentumTracking();
+    setMomentumID(requestAnimationFrame(momentumLoop));
+  }
+
+  function cancelMomentumTracking() {
+    if (momentumID) {
+      cancelAnimationFrame(momentumID);
+    }
+  }
+
+  function momentumLoop() {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += velX;
+
+      setVelX((prev) => {
+        const newValue = prev * 0.85;
+
+        if (Math.abs(newValue) > 0.5) {
+          setMomentumID(requestAnimationFrame(momentumLoop));
+        }
+
+        return newValue;
+      });
+    }
+  }
+
   return (
     <SimpleBar
       className={classNames(styles.casesList, className)}
       forceVisible="x"
       autoHide={false}
+      scrollableNodeProps={{ ref: containerRef }}
+      onMouseDown={(e) => {
+        if (e.button === 1) {
+          return;
+        }
+
+        if (containerRef.current) {
+          setIsDown(true);
+          setStartX(e.pageX - containerRef.current?.offsetLeft);
+          setScrollLeft(containerRef.current.scrollLeft);
+          containerRef.current.style.cursor = "grabbing";
+        }
+
+        cancelMomentumTracking();
+      }}
+      onMouseUp={() => {
+        setIsDown(false);
+
+        if (containerRef.current) {
+          containerRef.current.style.cursor = "grab";
+        }
+
+        beginMomentumTracking();
+      }}
+      onMouseMove={(e) => {
+        if (!isDown) {
+          return;
+        }
+
+        e.preventDefault();
+
+        if (containerRef.current && startX && scrollLeft !== null) {
+          const x = e.pageX - containerRef.current.offsetLeft;
+          const walkX = (x - startX) * 1;
+
+          const preventScrollLeft = containerRef.current.scrollLeft;
+          containerRef.current.scrollLeft = scrollLeft - walkX;
+          setVelX(containerRef.current.scrollLeft - preventScrollLeft);
+        }
+      }}
+      onWheel={cancelMomentumTracking}
     >
       {/* <div className={classNames(styles.casesList, className)} {...props}> */}
       {cases.map((project, index) => (
